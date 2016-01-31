0ac3be6c3d91d16081e5979bf220f545d36831e8
Daisuke Baba <baba.daisuke@gmail.com>
2016-01-31 17:47:37 +0900

    Add a new script allowing bitbake wget.py to download files from POODLE fixed servers

--- build.sh
+++ build.sh
@@ -4,12 +4,25 @@
 
 SCRIPT_BASE=$(dirname $(readlink -f -- "$0"))
 EDISON_SRC=${SCRIPT_BASE}/edison/edison-src
+POKY_DIR="${EDISON_SRC}/out/linux64/poky"
+
+BBCONF_REP="FETCHCMD_wget = \"/usr/bin/env wget -t 2 -T 30 -nv --passive-ftp --no-check-certificate\""
+BBCONF_REP_WITH="FETCHCMD_wget = \"/usr/bin/env /home/vagrant/fetch_cmd\""
 
 cd ${EDISON_SRC}
 cd ./out/linux64
 source poky/oe-init-build-env
 # moved to ./build implicitly
 touch conf/sanity.conf
+
+# Modify FETCHCMD_wget as GNU wget failed to connect POODLE vulnerability fixed servers
+if [ ! -f "${POKY_DIR}/meta/conf/bitbake.conf.org" ]; then
+  cp -f ${POKY_DIR}/meta/conf/bitbake.conf ${POKY_DIR}/meta/conf/bitbake.conf.org
+  echo -e "\033[93mbitbake.conf backup has been created => ${POKY_DIR}/meta/conf/bitbake.conf.org \033[0m"
+fi
+sed -i -e "s/${BBCONF_REP//\//\\/}/${BBCONF_REP_WITH//\//\\/}/g" ${POKY_DIR}/meta/conf/bitbake.conf
+
+# START BANNER
 echo -e "\033[92mStart building at $(date)\033[0m"
 time bitbake edison-image
 echo -e "\033[92mTerminated building at $(date)\033[0m"
